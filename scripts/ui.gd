48c367ad58f14761d2082c135726102570f098b6
AaronW <91666646+Duck2033@users.noreply.github.com>
2026-06-23 18:24:49 +1000

    Bleh

---
+++ scripts/ui.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+@onready var button_1 = $PanelContainer/MarginContainer/VBoxContainer/Employees
+@onready var button_2 = $PanelContainer/MarginContainer/VBoxContainer/Shop
+@onready var button_3 = $PanelContainer/MarginContainer/VBoxContainer/Market
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	pass
