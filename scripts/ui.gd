b6d7a87933eba26d868cc306708e6330f4434ead
AaronW <91666646+Duck2033@users.noreply.github.com>
2026-06-24 11:05:03 +1000

    Shop page and employee page now have home buttons, connected to main menu

--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,15 +1,78 @@
 extends CanvasLayer
-@onready var button_1 = $PanelContainer/MarginContainer/VBoxContainer/Employees
-@onready var button_2 = $PanelContainer/MarginContainer/VBoxContainer/Shop
-@onready var button_3 = $PanelContainer/MarginContainer/VBoxContainer/Market
 
+# --- MENUS ---
+@onready var main_menu = $PanelContainer/MarginContainer/MainMenu
+@onready var interface_1 = $PanelContainer/MarginContainer/ShopPage
+@onready var interface_2 = $PanelContainer/MarginContainer/EmployeePage
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+# --- BUTTONS ---
+@onready var btn_menu_1 = $PanelContainer/MarginContainer/MainMenu/ShopBtn
+@onready var btn_menu_2 = $PanelContainer/MarginContainer/MainMenu/EmployeesBtn
+#@onready var btn_nest = $PanelContainer/MarginContainer/Interface1/NestedButton
 
-	pass # Replace with function body.
+# Grab the navigation buttons (Assuming you named them consistently)
+#@onready var back_btns = [
+	#$PanelContainer/MarginContainer/Interface1/TopBar/BtnBack,
+	#$PanelContainer/MarginContainer/Interface1A/TopBar/BtnBack
+#]
+@onready var home_btns = [
+	$PanelContainer/MarginContainer/ShopPage/TopBar/BtnHome,
+	$PanelContainer/MarginContainer/EmployeePage/TopBar/BtnHome
+	#$PanelContainer/MarginContainer/Interface1A/TopBar/BtnHome
+]
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+# This array keeps track of our breadcrumbs!
+var menu_history : Array[Control] = []
+var current_menu : Control
+
+func _ready():
+	# 1. Connect Main Menu Buttons
+	btn_menu_1.pressed.connect(func(): open_page(interface_1))
+	btn_menu_2.pressed.connect(func(): open_page(interface_2))
+	#btn_nest.pressed.connect(func(): open_page(interface_1a))
 	
-	pass
+	# 2. Connect all Back and Home buttons via loops to save typing
+	#for btn in back_btns:
+		#btn.pressed.connect(go_back)
+	for btn in home_btns:
+		btn.pressed.connect(go_home)
+		
+	# 3. Start the game on the Main Menu
+	go_home()
+
+# --- NAVIGATION LOGIC ---
+
+func open_page(new_page: Control):
+	print("Attempting to open: ", new_page.name) # ADD THIS LINE
+	# If we are currently on a menu, add it to our history stack
+	if current_menu != null:
+		menu_history.append(current_menu)
+		current_menu.hide()
+		
+	# Show the new page
+	current_menu = new_page
+	current_menu.show()
+
+func go_back():
+	# If we have a history to go back to...
+	if menu_history.size() > 0:
+		current_menu.hide()
+		# Pop the last page off the end of the array and show it
+		current_menu = menu_history.pop_back()
+		current_menu.show()
+	else:
+		# If no history is left, just go home
+		go_home()
+
+func go_home():
+	# Clear the history because we are back at the start
+	menu_history.clear()
+	
+	# Hide all menus
+	main_menu.hide()
+	interface_1.hide()
+	interface_2.hide()
+	
+	# Show only the main menu
+	current_menu = main_menu
+	current_menu.show()
